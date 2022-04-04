bb537d5989aeaf8ddd5d8a869154cff6b93c1450
diegoMayorga <diego.mayorga86@gmail.com>
2022-04-04 13:53:43 -0300

    agreglo arreglo para que padre espere a todos sus hijos

--- fork/sumapares.py
+++ fork/sumapares.py
@@ -2,6 +2,8 @@ import sys
 import os
 import time
 
+processes = []
+
 def suma_pares():
 		i = 0
 		suma = 0
@@ -14,20 +16,26 @@ def suma_pares():
 
 def childs_function(n_childs):
 		for n in range(int(n_childs)):
-				if os.fork() == 0:
-						resultado = suma_pares()
-						print("%d - %d: %d" % (os.getpid(), os.getppid(),resultado))
-						exit(0)
+			pid = os.fork()
+			if pid == 0:
+					resultado = suma_pares()
+					print("%d - %d: %d" % (os.getpid(), os.getppid(),resultado))
+					os._exit(0)
+			else:
+					processes.append(pid)
 
 
 def childs_function_v(n_childs):
 		for n in range(int(n_childs)):
-				if os.fork() == 0:
+				pid = os.fork()
+				if pid == 0:
 						print("Starting process %d" % (os.getpid()))
 						resultado = suma_pares()
 						print("%d - %d: %d" % (os.getpid(), os.getppid(),resultado))
 						print("Ending process %d" % (os.getpid()))
-						exit(0)
+						os._exit(0)
+				else:
+						processes.append(pid)
 
 
 def main():
@@ -39,19 +47,25 @@ def main():
 				print("\t-n entero indica cantidad de hijos ")
 				print("\t-v indica modo verboso")
 				print("\t-h ayuda")
-				exit(0)
+				os._exit(0)
 		elif len(elements) == 2:
 				childs_to_create = elements[1]
 				childs_function(childs_to_create)
-				pid,estado = os.wait()
+				while processes:
+					pid, exit_code = os.wait()
+					if pid != 0:
+							processes.remove(pid)
 		elif len(elements) == 3 and elements[2] == "-v":
 				childs_to_create = elements[1]
 				childs_function_v(childs_to_create)
-				pid,estado = os.wait()				
+				while processes:
+					pid, exit_code = os.wait()
+					if pid != 0:
+							processes.remove(pid)		
 		else:
 				print("Error en la cantidad de argumentos o argumentos invalidos.")
 				print("Argumentos validos: \n\t-h (ayuda) \n\t-n 1 (cant. hijos) \n\t-v (verbose)")
-				exit(0)
+				os._exit(0)
 
 
 if __name__ == "__main__":
