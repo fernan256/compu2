3613b4d1db74385866a67bf755d7a64f4f920c93
diegoMayorga <diego.mayorga86@gmail.com>
2022-04-22 18:44:28 -0300

    arreglo segun videos clase martes pasado

--- pipe/invertir.py
+++ pipe/invertir.py
@@ -1,74 +1,52 @@
 import os
 import argparse
-import time, sys
 
-processes = []
 
 def childs_function(file_directory):
-	fileObject = open(file_directory, "r")
 	with open(file_directory, "r") as file1:
 		total_lines = sum(1 for line in file1)
+	fileObject = open(file_directory, "r")
+	lines = fileObject.readlines()
 
 	r,w = os.pipe()
 	r_child,w_child = os.pipe()
-
-	for lineR in fileObject:
+	
+	for n in range(total_lines):
 		pid = os.fork()
-		# if pid:
-		# 	os.close(w_child)
-		# 	os.close(r)
-		# 	r_child = os.fdopen(r_child)
-		# 	print("padre leyendo...")
-		# 	while True:
-		# 		line = r_child.readline()
-		# 		if line:
-		# 			print("Padre leyendo: %s" % line)
-		# 		else:
-		# 			os.wait()
-		# 			print("Terminando padre...")
-		# 			sys.exit(0)
-		# else:
-		# 	os.close(w)
-		# 	os.close(r_child)
-		# 	w_child = os.fdopen(w_child,'w')
-		# 	print("Hijo escribiendo...")
-		# 	lineLenght = len(lineR)
-		# 	lineReverted = lineR[lineLenght::-1]
-		# 	w_child.write(f"{os.getpid()} + {lineReverted}")
-		# 	w_child.flush()
-		# 	print("Encontrado EOF en el stdin...")
-		# 	# w_child.close()
-		# 	# time.sleep(3)
-		# 	print("Hijo terminando...")
 		if pid == 0:
-			os.close(r_child)
 			os.close(w)
+			os.close(r_child)
+			r = os.fdopen(r, 'r')
+			lineR = r.read()
 			w_child = os.fdopen(w_child,'w')
-			lineLenght = len(lineR)
-			lineReverted = lineR[lineLenght::-1]
-			print(f"{os.getpid()} + {lineReverted}")
+			lineReverted = lineR[::-1]
+			w_child.write(f"{lineReverted}")
+			w_child.flush()
+			w_child.close()
 			os._exit(0)
-		else:
-			os.close(w_child)
-			os.close(r)
-			r_child = os.fdopen(r_child)
-			line = r_child.readline()
-			if line:
-				print("Padre leyendo: %s" % line)
-			else:
-				processes.append(pid)
-				# sys.exit(0)
+
+	os.close(r)
+	os.close(w_child)
+	w = os.fdopen(w, 'w')
+
+	for l in lines:
+		w.write(l)
+
+	w.close()
+
+	r_child = os.fdopen(r_child)
+	while True:
+		line = r_child.read()
+		if len(line) == 0:
+			break
+		print(line)
 
 
 def main(args):
     variables = vars(args)
 
     childs_function(variables['f'])
-    
-    while processes:
-      pid, exit_code = os.wait()
-      if pid != 0:
-          processes.remove(pid)
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
