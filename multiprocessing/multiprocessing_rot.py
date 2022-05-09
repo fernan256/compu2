8a81f1d5499cfefda000e67188e25c9bb204f473
diegoMayorga <diego.mayorga86@gmail.com>
2022-05-09 20:43:23 -0300

    arreglos finales

--- multiprocessing/multiprocessing_rot.py
+++ multiprocessing/multiprocessing_rot.py
@@ -11,49 +11,49 @@ def rot13(value):
         else:
             Rot13 += i
     return Rot13
-        
 
 
-def escritor(q,pipe_w):
-    print("Escritor escribiendo %d" % os.getpid())
+def show_message(q):
+    msg = q.get()
+    print("\nEscritor mostrando mensaje encriptado: %s" % msg)
+    return
+
+
+def writer(q,w):
     sys.stdin = open(0)
     while True:
-        try:
-            print("Ingrese una linea: ")
-            escrito = sys.stdin.readline()
-            if escrito == "exit":
-                break
-            pipe_w.send(escrito)
-            while q:
-                # try:
-                msg = q.get()
-                print("Escritor leyendo encriptado: %s" % msg)
-                break
-        except IOError:
-            pass
+        print("Ingrese una linea: ")
+        escrito = sys.stdin.readline()
+        w.send(escrito)
+        show_message(q)
+        break
     
 
 
-def lector(q,pipe_r):
-    valor = pipe_r.recv()
-    print(f"valor: {valor}")
-    if valor:
-        encrypted = rot13(valor)
-        q.put(encrypted)
-        pipe_r.send("Ya lei...")
+def reader(q,r):
+    valor = r.recv()
+    encrypted = rot13(valor)
+    q.put(encrypted)
 
 
 
 def main():
-    r,w = Pipe()
+    r, w = Pipe()
     q = Queue()
 
-    p1 = Process(target=escritor, args=(q,w))
-    p2 = Process(target=lector, args=(q,r))
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.join()
+    processes = []
+
+    p1 = Process(target=writer, args=(q, w,))
+    p2 = Process(target=reader, args=(q, r,))
+
+    processes.extend([p1, p2])
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+    
     r.close()
     w.close()
     print("Padre terminando...")
