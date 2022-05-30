4bd0d0427dd4bf4e2cd21c27119f40b0cf32e270
diegoMayorga <diego.mayorga86@gmail.com>
2022-05-30 20:23:38 -0300

    agrego ejercicio sobre threads

--- threading/threading_rot.py
+++ threading/threading_rot.py
@@ -1,11 +1,5 @@
-from ast import arg
-from binascii import b2a_hqx
 import threading, queue
-import os, sys
-import threading
-
-
-# check = threading.Condition()
+import sys
 
 
 def rot13(value):
@@ -28,16 +22,7 @@ def show_message(q):
 def writer(q,q2):
     sys.stdin = open(0)
     print("Ingrese una linea: ")
-    # check.acquire()
     getUserEnter = sys.stdin.readline()
-    print(getUserEnter)
-
-    # escrito = getUserEnter
-    # check.wait()
-    # if escrito:
-    #     print(f'estoy {escrito}')
-    #     check.notify()
-    #     check.release()
     q2.put(getUserEnter)
     while q:
         show_message(q)
@@ -47,30 +32,21 @@ def writer(q,q2):
 
 
 def reader(q,q2):
-    # if escrito:
-    # check.acquire()
     escrito = q2.get()
-    print(f'aa {escrito}')
     encrypted = rot13(escrito)
     q.put(encrypted)
-    # check.notify()
-    # check.release()
 
 
 
 def main():
-    escrito = ""
     q = queue.Queue()
     q2 = queue.Queue()
 
-    a = threading.Thread(target=writer, args=(q,q2 ))
-    b = threading.Thread(target=reader, args=(q,q2 ))
+    writerThread = threading.Thread(target=writer, args=(q,q2 ))
+    readerThread = threading.Thread(target=reader, args=(q,q2 ))
  
-    # Now start both threads
-    a.start()
-    b.start()
-    # a.join()
-    # b.join()
+    writerThread.start()
+    readerThread.start()
     q.join()
     q2.join()
 
